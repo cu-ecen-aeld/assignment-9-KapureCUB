909ad4c1ee4e60c932f1f839552bc1e9905aef0d
deepak <deepak@Ubuntu.myguest.virtualbox.org>
2024-02-11 13:12:02 -0700

    changed .mk for final assignment3 changes

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 4706a275be2809cb0b567191b89155bbf534ddb8 
+AESD_ASSIGNMENTS_VERSION = cd63a84dbd246c0b784b62be265e78973ef93094 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
