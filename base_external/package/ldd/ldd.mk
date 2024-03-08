6f0c107cc9c958d30deccf698d926fdedfb3fdbb
deepak <deepak@Ubuntu.myguest.virtualbox.org>
2024-03-07 19:22:40 -0700

    updated package dir to include ldd

---
+++ base_external/package/ldd/ldd.mk
@@ -0,0 +1,48 @@
+##############################################################
+#
+# LDD
+#
+##############################################################
+
+#module variables
+SCULL_MODULE = scull
+MISC_MODULE = misc-modules
+HELLO_MODULE = hello
+
+LDD_VERSION = cbd43ee59b9bc20408f5c7a1e82d90cdec8db16c 
+LDD_SITE = git@github.com:cu-ecen-aeld/assignment-7-KapureCUB.git
+LDD_SITE_METHOD = git
+LDD_GIT_SUBMODULES = YES
+LDD_MODULE_SUBDIRS = $(MISC_MODULE)
+LDD_MODULE_SUBDIRS += $(SCULL_MODULE)
+
+#path variables 
+AESD_MODULES_DIR = /lib/modules/aesd-modules
+
+define LDD_BUILD_CMDS
+        $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/misc-modules all
+        $(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/scull all
+endef
+
+define LDD_INSTALL_TARGET_CMDS
+        #install .ko, load and unload scripts to AESD_MODULES_DIR
+        $(INSTALL) -d 0755 $(TARGET_DIR)$(AESD_MODULES_DIR)
+	
+	#misc_modules
+	$(INSTALL) -d 0755 $(TARGET_DIR)$(AESD_MODULES_DIR)/$(MISC_MODULE)
+        $(INSTALL) -m 0755 $(@D)/$(MISC_MODULE)/$(MISC_MODULE).ko $(TARGET_DIR)$(AESD_MODULES_DIR)/$(MISC_MODULE)
+	$(INSTALL) -m 0755 $(@D)/$(MISC_MODULE)/$(HELLO_MODULE).ko $(TARGET_DIR)$(AESD_MODULES_DIR)/$(MISC_MODULE)
+	$(INSTALL) -m 0755 $(@D)/$(MISC_MODULE)/modules_load $(TARGET_DIR)$(AESD_MODULES_DIR)/$(MISC_MODULE)
+	$(INSTALL) -m 0755 $(@D)/$(MISC_MODULE)/modules_unload $(TARGET_DIR)$(AESD_MODULES_DIR)/$(MISC_MODULE)
+
+        #scull
+        $(INSTALL) -d 0755 $(TARGET_DIR)$(AESD_MODULES_DIR)/$(SCULL_MODULE)
+        $(INSTALL) -m 0755 $(@D)/$(SCULL_MODULE)/*.ko $(TARGET_DIR)$(AESD_MODULES_DIR)/$(SCULL_MODULE)
+        $(INSTALL) -m 0755 $(@D)/$(SCULL_MODULE)/scull_load $(TARGET_DIR)$(AESD_MODULES_DIR)/$(SCULL_MODULE)
+        $(INSTALL) -m 0755 $(@D)/$(SCULL_MODULE)/scull_unload $(TARGET_DIR)$(AESD_MODULES_DIR)/$(SCULL_MODULE)
+	
+	#install rootfs_overlay
+	$(INSTALL) -m 0755 $(@D)/ $(TARGET_DIR)/etc/init.d/S99aesdsocket	
+endef
+$(eval $(kernel-module))
+$(eval $(generic-package))
