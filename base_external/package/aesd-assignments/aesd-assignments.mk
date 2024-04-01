06f2ee7e6eca9d01f38a74a0e656eab93fcd5229
deepak <deepak@Ubuntu.myguest.virtualbox.org>
2024-04-01 15:27:02 -0600

    updated commit for aesdsocket

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 0bab39ad01291fee912acb23ccea57f29f018e44 
+AESD_ASSIGNMENTS_VERSION =  4ccfd83bf8050eaabadff1f9ce2aacd7a111822a
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
--- base_external/package/aesdchar/aesdchar.mk
+++ base_external/package/aesdchar/aesdchar.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESDCHAR_VERSION = c5b5a507e45498bc04feb6cf73e522e918bbf649
+AESDCHAR_VERSION = 4ccfd83bf8050eaabadff1f9ce2aacd7a111822a 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
